e17e91c141959acb6586bf9524facda53e1faa9e
j-colson419 <jcolson419@gmail.com>
2019-07-29 12:03:46 -0400

    Fixed alignment of nav-bar with the rest of the view content

--- ng-pet-project/src/app/app-routing.module.ts
+++ ng-pet-project/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PetProfileComponent } from './pet-profile/pet-profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pet-profile',
+    component: PetProfileComponent
+  },
+  {
+    path: '',
+    component: PetProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
